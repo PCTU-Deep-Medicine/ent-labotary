f2c05fdd332c38343167a3456421bcd832b375eb
coung21 <coungdev@gmail.com>
2025-08-14 13:06:22 +0700

    Refactor model initialization and training parameters in ssl_dino.py

--- src/experiment/ssl_dino.py
+++ src/experiment/ssl_dino.py
@@ -3,7 +3,7 @@ import timm
 
 if __name__ == "__main__":
     model = timm.create_model(
-        "vit_small_patch16_224.augreg_in21k", pretrained=True
+        "vit_small_patch16_224.augreg_in21k", pretrained=True, dynamic_img_size=True
     )  # Load the model.
     lightly_train.train(
         out="outputs/ssl_dino/vit16s",  # Output directory.
@@ -12,11 +12,11 @@ if __name__ == "__main__":
         method="dino",  # Use DINO method.
         epochs=300,
         batch_size=32,
-        model_args={"dynamic_img_size": True},
+        # model_args={"dynamic_img_size": True},
         transform_args={
             "image_size": (224, 224),
         },
-        loggers={"wandb": {"project": "ent-endoscopy-ssl"}},
+        # loggers={"wandb": {"project": "ent-endoscopy-ssl"}},
         overwrite=True,  # Overwrite existing outputs.
     )
 
