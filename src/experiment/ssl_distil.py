901492890f0effd68ec8b452322c4eb112ba523d
coung21 <coungdev@gmail.com>
2025-08-15 23:08:56 +0700

    ssl distilation module

---
+++ src/experiment/ssl_distil.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import lightly_train
+import timm
+
+# import torch.nn as nn
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)  # noqa: E402
+
+from utils.upload_ckpt import upload_checkpoint  # noqa: E402
+
+if __name__ == "__main__":
+    model = timm.create_model(
+        "vit_small_patch16_224.augreg_in21k",
+        pretrained=True,
+        dynamic_img_size=True,
+    )  # Load the model.
+
+    # if isinstance(getattr(model, "global_pool", None), str):
+    #     model.global_pool = nn.AdaptiveAvgPool2d(1)  # giờ _pool sẽ callable
+    lightly_train.train(
+        out="outputs/ssl_distil/vit16s",  # Output directory.
+        data="data/kyucapsule",  # Directory with images.
+        model=model,  # Pass theH", "d TIMM model.
+        method="distillation",  # Use DINO method.
+        epochs=300,
+        batch_size=32,
+        transform_args={
+            "image_size": (224, 224),
+            # "local_view": {"num_views": 0},  # <-- TẮT LOCAL CROPS
+        },
+        method_args={
+            "teacher": "dinov3/vitb16",
+            # Replace with your own url
+            "teacher_url": os.environ.get("DINOV3_TEACHER_URL", "default_value"),
+        },
+        loggers={"wandb": {"project": "ent-endoscopy-ssl"}},
+        num_workers=64,
+        resume_interrupted=True,
+        overwrite=True,  # Overwrite existing outputs.
+    )
+
+    lightly_train.export(
+        out="outputs/ssl_distil/vit16s/vit16s_patch16_224_distil.pth",
+        checkpoint="outputs/ssl_distil/vit16s/checkpoints/last.ckpt",
+        part="model",
+        format="torch_state_dict",
+    )
+
+    upload_checkpoint(repo_id="coung21/ent", folder_path="outputs")
