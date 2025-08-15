2f1121b6eaa2d31943926f6de34f78687ba0044c
coung21 <coungdev@gmail.com>
2025-08-15 11:50:23 +0700

    Add num_workers parameter to training configuration in ssl_dino.py

--- src/experiment/ssl_dino.py
+++ src/experiment/ssl_dino.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
             "local_view": {"num_views": 0},  # <-- TẮT LOCAL CROPS
         },
         loggers={"wandb": {"project": "ent-endoscopy-ssl"}},
+        num_workers=1,
         resume=True,
         overwrite=True,  # Overwrite existing outputs.
     )
