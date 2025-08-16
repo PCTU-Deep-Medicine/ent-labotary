63738de5bccd21a379ac3013f35bd83de7188929
coung21 <coungdev@gmail.com>
2025-08-17 02:23:59 +0700

    Update ignore patterns in upload_checkpoint function to exclude additional file types

--- src/utils/upload_ckpt.py
+++ src/utils/upload_ckpt.py
@@ -6,5 +6,15 @@ def upload_checkpoint(repo_id: str, folder_path: str):
         repo_id=repo_id,
         folder_path=folder_path,
         repo_type="model",  # thường là "model",
-        ignore_patterns=["checkpoints/**"],
+        ignore_patterns=[
+            "**/wandb/**",
+            "**/*.wandb",
+            "**/events.out.tfevents.*",
+            "**/*.tfevents*",
+            "checkpoints/**",
+            "**/.DS_Store",
+            "**/__pycache__/**",
+            "**/*.log",
+            "**/*.tmp",
+        ],
     )
