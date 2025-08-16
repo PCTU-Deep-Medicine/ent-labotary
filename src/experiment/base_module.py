4157c4980b982b91986155944de6c852152ddebe
coung21 <coungdev@gmail.com>
2025-08-16 14:09:26 +0700

    Update learning rate in configure_optimizers from 1e-2 to 1e-3

--- src/experiment/base_module.py
+++ src/experiment/base_module.py
@@ -98,7 +98,7 @@ class BaseModule(pl.LightningModule):
 
     # ──────────────────────────── optimizer ──────────────────────────────
     def configure_optimizers(self):
-        opt = torch.optim.AdamW(self.parameters(), lr=1e-2, weight_decay=1e-2)
+        opt = torch.optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-2)
         warmup = LinearLR(opt, start_factor=0.1, total_iters=10)
         step = StepLR(opt, step_size=90, gamma=0.1)
         scheduler = SequentialLR(opt, schedulers=[warmup, step], milestones=[10])
