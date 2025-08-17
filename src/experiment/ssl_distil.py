c4fb45fb66ef7f1ffbb469ff55154438c0020f70
coung21 <coungdev@gmail.com>
2025-08-17 23:29:27 +0700

    Update model to ConvNeXt V2 Tiny and adjust output paths in ssl_distil.py

--- src/experiment/ssl_distil.py
+++ src/experiment/ssl_distil.py
@@ -14,7 +14,7 @@ from utils.upload_ckpt import upload_checkpoint  # noqa: E402
 
 if __name__ == "__main__":
     model = timm.create_model(
-        "resnet50",
+        "convnextv2_tiny",
         pretrained=True,
         # dynamic_img_size=True,
     )  # Load the model.
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # if isinstance(getattr(model, "global_pool", None), str):
     #     model.global_pool = nn.AdaptiveAvgPool2d(1)  # giờ _pool sẽ callable
     lightly_train.train(
-        out="outputs/ssl_distil2/resnet50",  # Output directory.
+        out="outputs/ssl_distil2/convnextv2_tiny",  # Output directory.
         data="data/12endo/train",  # Directory with images.
         model=model,  # Pass theH", "d TIMM model.
         method="distillation",  # Use DINO method.
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     )
 
     lightly_train.export(
-        out="outputs/ssl_distil2/resnet50/resnet50_distil.pth",
-        checkpoint="outputs/ssl_distil2/resnet50/checkpoints/last.ckpt",
+        out="outputs/ssl_distil2/convnextv2_tiny/convnextv2_tiny_distil.pth",
+        checkpoint="outputs/ssl_distil2/convnextv2_tiny/checkpoints/last.ckpt",
         part="model",
         format="torch_state_dict",
     )
