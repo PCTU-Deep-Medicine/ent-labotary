4b1c6375adc3dbaebbadcffb1ffe5a64a190e06d
coung21 <coungdev@gmail.com>
2025-08-21 18:33:17 +0700

    Add setup script for environment initialization and dependencies

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+mkdir outputs
+export HF_TOKEN=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
+export DINOV3_TEACHER_URL='https://dinov3.llamameta.net/dinov3_vitb16/dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoicGhjdzV5anNzeG96OG1vdHdqenc5dGxqIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZGlub3YzLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3NTU4MzYxODh9fX1dfQ__&Signature=QqMep2UH3Bgdo3tWYNX7hA9jKn4eqZkc-C1HgjFPWz9%7EgxB14IhnGZpcS-SynMPjn1R4gRX-YngpOJIW46FKvcdjKE38v7Tp2CDbm1DKW8G3ykjper3XkXKTtjnpQFlQnftSV8bNSeNrjHc1aCK1j2m4hy9jN0w1Y5vvcnLXNhvGz%7E4E43ce0hCMY-cfJC%7EYXTawGrWnAQto9oThOxyQwJaQfMUE5XY7WWRCHlN-0v2hMbmR5EleGwdWkHpM3kH6SqF6SyLGjxjSv2XF1L3x2jrulAhS%7EbdLH-Ef7s3-tMyI0VLTbPyqEpDEaO9L1%7EePDYQZwnLxJZsBCvtRU8sQpQ__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=752086624458100'
+pip install -r requirements.txt
+pip install --pre --upgrade --no-cache-dir torch --extra-index-url https://download.pytorch.org/whl/nightly/cu128
+pip install --pre --upgrade --no-cache-dir torchvision --extra-index-url https://download.pytorch.org/whl/nightly/cu128
+sh scripts/prepare_data.sh
+python3 scripts/split.py
+# 45dfee4504ac5fab852dfb6a3d5af96af6f6c67b
+# git clone https://github.com/PCTU-Deep-Medicine/ent-labotary.git
